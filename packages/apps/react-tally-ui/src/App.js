b168f0e74d28815014a56e2076acbf1ec1a26463
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-06 12:18:14 +0530

    Updated the peerDependency in the @glassball/excel library

--- packages/apps/react-tally-ui/src/App.js
+++ packages/apps/react-tally-ui/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import TaggableTable from "./components/taggableTable";
+import {excelToJson} from "@glassball/excel";
 
 function App() {
   return (
     <div className="App">
       <h1>React Tally</h1>
       <div>
-        <TaggableTable />
+        <input type="file" />
+      </div>
+      <div>
+        {/*<TaggableTable />*/}
       </div>
     </div>
   );
