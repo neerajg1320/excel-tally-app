94ab33ec411ec9a183f31772d322eb37e46e8a85
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-07 12:42:38 +0530

    The electron communication library is working

--- packages/apps/electron-tally/src/index.js
+++ packages/apps/electron-tally/src/index.js
@@ -16,8 +16,11 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
+      // preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      // enableRemoteModule: true,
+      contextIsolation: false
+    }
   });
 
   // and load the index.html of the app.
--- packages/apps/react-tally-ui/src/App.js
+++ packages/apps/react-tally-ui/src/App.js
@@ -2,8 +2,19 @@ import logo from './logo.svg';
 // import './App.css';
 import TaggableTable from "./components/taggableTable";
 import FileReader from "./components/fileReader/FileReader";
+import {remoteCall, removeListeners} from "@glassball/electron-client-remote";
+import {useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    console.log(`App: First Render`);
+    remoteCall('ui:start', 'STARTED');
+    
+    return () => {
+      removeListeners();
+      console.log(`App: Destroyed`);
+    }
+  });
 
   return (
     <div className="App">
