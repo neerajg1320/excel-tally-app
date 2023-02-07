d63faa678f7844a7ca55f15a8ea0dbfc38ff3a39
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-07 16:54:06 +0530

    Added commands to run in dev and production mode

--- run.cmd
+++ run.cmd
@@ -1,7 +1,13 @@
+# Clone repo
+git clone ...
+
 # Development mode
+lerna bootstrap
 npm start
 
 # Production mode
+mkdir packages/apps/electron-tally/node_modules
+lerna bootstrap
 npm run build
 
 $ cd packages/apps/electron-tally/dist
